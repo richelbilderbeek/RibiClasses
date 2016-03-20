00a99bd79f933cc4af24cbad90ef47f74f8ca118
richelbilderbeek <richel@richelbilderbeek.nl>
2016-03-20 14:12:12 +0100

    Show coverage

--- build.sh
+++ build.sh
@@ -5,3 +5,13 @@ cd RibiClasses
 qmake RibiClassesTest.pro
 make
 ./RibiClassesTest
+gcov-5 main_test.cpp
+#gcov-5 my_functions.cpp
+# Create gcov files
+#for filename in `ls *.cpp`; do gcov $filename; done
+#for filename in `ls *.h`; do gcov $filename; done
+
+# Display gcov files
+#for filename in `ls *.h.gcov`; do cat $filename; done
+#for filename in `ls *.cpp.gcov`; do cat $filename; done
+
