4778c4c6f3962ddafd551405ca8ddceec530dbf4
richelbilderbeek <richel@richelbilderbeek.nl>
2016-03-24 10:03:49 +0100

    Added codecov?

--- build.sh
+++ build.sh
@@ -5,12 +5,3 @@ cd RibiClasses
 qmake RibiClassesTest.pro
 make
 ./RibiClassesTest
-#gcov-5 main_test.cpp
-#gcov-5 my_functions.cpp
-# Create gcov files
-for filename in `find . -name '*.cpp'`; do gcov-5 $filename; done
-for filename in `find . -name '*.h'`; do gcov-5 $filename; done
-# Display gcov files
-for filename in `find . -name '*.cpp.gcov'`; do cat $filename; done
-for filename in `find . -name '*.cpp.gcov'`; do cat $filename; done
-
