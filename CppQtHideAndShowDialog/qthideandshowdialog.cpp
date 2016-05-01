5d3a44454e78279cf76e4345c2bab1534761867c
richelbilderbeek <richel@richelbilderbeek.nl>
2016-05-01 18:59:19 +0200

    EOD

--- CppQtHideAndShowDialog/qthideandshowdialog.cpp
+++ CppQtHideAndShowDialog/qthideandshowdialog.cpp
@@ -25,6 +25,7 @@ along with this program.If not, see <http://www.gnu.org/licenses/>.
 #include "qthideandshowdialog.h"
 
 #include <cassert>
+#include <QApplication>
 #include <QKeyEvent>
 
 #include "trace.h"
