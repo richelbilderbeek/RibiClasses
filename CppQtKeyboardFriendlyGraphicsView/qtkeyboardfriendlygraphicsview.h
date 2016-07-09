ee444a65412f831b261a47d2ef858be8f39c368d
richelbilderbeek <richel@richelbilderbeek.nl>
2016-07-09 13:27:15 +0200

    Added #include

--- CppQtKeyboardFriendlyGraphicsView/qtkeyboardfriendlygraphicsview.h
+++ CppQtKeyboardFriendlyGraphicsView/qtkeyboardfriendlygraphicsview.h
@@ -21,6 +21,7 @@ along with this program.If not, see <http://www.gnu.org/licenses/>.
 #ifndef QTKEYBOARDFRIENDLYGRAPHICSVIEW_H
 #define QTKEYBOARDFRIENDLYGRAPHICSVIEW_H
 
+#include <random>
 #include <QGraphicsView>
 
 struct QGraphicsItem;
