ba698c788b7f19ad4b9c1f9b42d1efad3c9a84b5
richelbilderbeek <richel@richelbilderbeek.nl>
2016-08-30 13:24:50 +0200

    Moved CppConceptMap and CppQtConceptMap to own GitHub

--- CppConceptMap/conceptmapexamplefactory_test.cpp
+++
@@ -1,9 +0,0 @@
-#include "conceptmapexamplefactory.h"
-
-#include <boost/test/unit_test.hpp>
-
-BOOST_AUTO_TEST_CASE(ribi_concept_map_example_factory_test)
-{
-  ribi::cmap::ExampleFactory f;
-  BOOST_CHECK(!f.GetNastyTests().empty());
-}
--- CppConceptMap/conceptmapfactory_test.cpp
+++
@@ -1,8 +0,0 @@
-#include "conceptmapfactory.h"
-
-#include <boost/test/unit_test.hpp>
-
-BOOST_AUTO_TEST_CASE(ribi_concept_map_concept_map_factory_test)
-{
-  const ribi::cmap::ConceptMapFactory f;
-}
