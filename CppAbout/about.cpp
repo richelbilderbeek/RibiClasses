faa13cb661366353a720c03176803325975f1109
richelbilderbeek <richel@richelbilderbeek.nl>
2016-11-06 18:22:24 +0100

    Removed copyright in comments

--- CppAbout/about.cpp
+++ CppAbout/about.cpp
@@ -86,17 +86,17 @@ std::vector<std::string> ribi::About::CreateLicenceText() const noexcept
     m_file_title + ", " + m_file_description,
     "Copyright (C) " + m_years + " " + m_author,
     "",
-    "This program is free software: you can redistribute it and/or modify",
-    "it under the terms of the GNU General Public License as published by",
-    "the Free Software Foundation, either version 3 of the License, or",
-    "(at your option) any later version.",
     "",
-    "This program is distributed in the hope that it will be useful,",
-    "but WITHOUT ANY WARRANTY; without even the implied warranty of",
-    "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the",
-    "GNU General Public License for more details.",
-    "You should have received a copy of the GNU General Public License",
-    "along with this program. If not, see <http://www.gnu.org/licenses/>."
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    ""
   };
   return v;
 }
--- CppFuzzy_equal_to/fuzzy_equal_to.h
+++ CppFuzzy_equal_to/fuzzy_equal_to.h
@@ -1,23 +1,3 @@
-//---------------------------------------------------------------------------
-/*
-fuzzy_equal_to, fuzzy equal_to predicate
-Copyright (C) 2011-2016 Richel Bilderbeek
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-GNU General Public License for more details.
-You should have received a copy of the GNU General Public License
-along with this program. If not, see <http://www.gnu.org/licenses/>.
-*/
-//---------------------------------------------------------------------------
-//From http://www.richelbilderbeek.nl/CppFuzzy_equal_to.htm
-//---------------------------------------------------------------------------
 #ifndef RIBI_FUZZY_EQUAL_TO_H
 #define RIBI_FUZZY_EQUAL_TO_H
 
