864321d3ec6c8473f4bae492b56d5fa44368736c
richelbilderbeek <richel@richelbilderbeek.nl>
2017-02-22 17:11:54 +0100

    Removed dependency on TestTimer

--- CppMultiVector/multivector.cpp
+++ CppMultiVector/multivector.cpp
@@ -1,25 +1,4 @@
-//---------------------------------------------------------------------------
-/*
-MultiVector, any-dimensional std::vector class
-Copyright (C) 2011-2015 Richel Bilderbeek
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS  A PARTICULAR PURPOSE.See the
-GNU General Public License for more details.
-You should have received a copy of the GNU General Public License
-along with this program.If not, see <http://www.gnu.org/licenses/>.
-*/
-//---------------------------------------------------------------------------
-//From http://www.richelbilderbeek.nl/CppMultiVector.htm
-//---------------------------------------------------------------------------
 #include "multivector.h"
-#include "testtimer.h"
 
 #ifndef NDEBUG
 void ribi::TestMultiVector() noexcept
@@ -32,7 +11,6 @@ void ribi::TestMultiVector() noexcept
   {
 
   }
-  const TestTimer test_timer(__func__,__FILE__,1.0);
   ribi::MultiVector<int> x;
   //1 dimensional
   x.Store( { 0 }, 0);
