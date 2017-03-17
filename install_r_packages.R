43fcacf7efe8d70b4d07f73dc78d0ace71839f15
richelbilderbeek <richel@richelbilderbeek.nl>
2017-03-17 10:19:21 +0100

    Use 'lib <- ~/R

--- install_r_packages.R
+++ install_r_packages.R
@@ -1,13 +1,15 @@
 # Installs the R packages needed
-install.packages("devtools", repos = "http://cran.uk.r-project.org")
+lib_path <- "~/R"
+
+install.packages("devtools", repos = "http://cran.uk.r-project.org", lib = lib_path)
 
 # v1.2, does not support Endemic&NonEndemic yet
-install.packages("DAISIE", repos = "http://cran.uk.r-project.org")
+install.packages("DAISIE", repos = "http://cran.uk.r-project.org", lib = lib_path)
 # v1.3, does support Endemic&NonEndemic, but is closed
 # devtools::install_github("richelbilderbeek/DAISIE_1_3", subdir = "DAISIE")
 
-devtools::install_github("richelbilderbeek/nLTT")
-devtools::install_github("rsetienne/PBD", subdir = "PBD")
+devtools::install_github("richelbilderbeek/nLTT", lib = lib_path)
+devtools::install_github("rsetienne/PBD", subdir = "PBD", lib = lib_path)
 warnings()
 
 #packageVersion("DAISIE")
